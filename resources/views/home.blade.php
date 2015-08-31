9b0094c0a454e06748a2a094ff1550223905d2a9
Rai26 <rai26k@googlemail.com>
2015-08-31 13:54:20 +0200

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
             <div class="row">
                 <div class="one-half column welcome-message">
                     <h3>Welcome to</h3>
-                    <img class="logo u-max-full-width" src="{{ asset("img/bns-logo.png") }}">
+                    <img class="logo u-max-full-width" src="{{ asset("img/bns-logo-small.png") }}">
                     <h3>Database</h3>
                 </div>
                 <div class="one-half column">
-                    <img class="figure-1" src="{{ asset("img/figure-1.png")  }}">
+                    <img class="figure-1" src="{{ asset("img/figure-1-small.png")  }}">
                 </div>
             </div>
         </div>
