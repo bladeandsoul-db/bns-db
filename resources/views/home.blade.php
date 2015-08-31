d2eb16a91b844ca714f0b4bb1eb6a31020264f8c
Rai26 <rai26k@googlemail.com>
2015-08-31 13:32:35 +0200

    typos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="one-half column welcome-message">
                     <h3>Welcome to</h3>
-                    <img class="logo u-max-full-width" src="{{ asset("img/BnS-Logo.png") }}">
+                    <img class="logo u-max-full-width" src="{{ asset("img/bns-logo.png") }}">
                     <h3>Database</h3>
                 </div>
                 <div class="one-half column">
